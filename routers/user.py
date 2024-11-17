41fca97733109d70ca2a3a1eec15f5e017de057f
asigi <asigin79@gmail.com>
2024-11-17 22:13:40 +0300

    get all_user

--- routers/user.py
+++ routers/user.py
@@ -5,6 +5,6 @@ from db.db import users_select_all
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.get("/all")
-def all_users():
+async def all_users():
     r = users_select_all()
     return {"status_code": status.HTTP_201_CREATED, "transaction": r}
