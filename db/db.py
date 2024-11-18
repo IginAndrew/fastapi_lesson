794c2a7af35136e3709b76d2beb7af5573aa8dbc
IginAndrew <asigin79@gmail.com>
2024-11-18 09:48:03 +0300

    index на работе

---
+++ db/db.py
@@ -0,0 +1,164 @@
+import sqlite3
+
+def get_connection():
+    try:
+        con = sqlite3.connect('db/journal.db')
+        con.row_factory = sqlite3.Row
+        print("Успешное подключение!")
+        return con
+    except Exception:
+        print("Ошибка подключения!")
+
+
+def users():
+    con = get_connection()
+    with con:
+        c = con.cursor()
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS Users
+    (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    login TEXT NOT NULL,
+    psw TEXT NOT NULL,
+    UNIQUE ("login") ON CONFLICT IGNORE
+    );
+    ''')
+    con.commit()
+
+
+def dates():
+    con = get_connection()
+    with con:
+        c = con.cursor()
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS Dates
+(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+date TEXT NOT NULL
+);
+    ''')
+    con.commit()
+
+
+def journals():
+    con = get_connection()
+    with con:
+        c = con.cursor()
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS Journals
+(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+diary TEXT,
+dates_id INTEGER,
+users_id INTEGER,
+FOREIGN KEY (dates_id) REFERENCES Dates(id),
+FOREIGN KEY (users_id) REFERENCES Users(id)
+);
+    ''')
+    con.commit()
+
+def users_insert(login,psw):
+    try:
+        con= get_connection()
+        with con:
+            c = con.cursor()
+        c.execute('''
+                  INSERT INTO Users (login,psw) values
+                  (?,?);
+                  ''',(login,psw,))
+        con.commit()
+    except sqlite3.Error as error:
+        print('Ошибка при работе sql', error)
+    finally:
+        if con:
+            con.close()
+            print('Соединение с sql закрыто')
+
+def dates_insert(dates):
+    try:
+        con= get_connection()
+        with con:
+            c = con.cursor()
+        c.execute('''
+                  INSERT INTO Dates (date) values
+                  (?);
+                  ''',(dates,))
+        con.commit()
+    except sqlite3.Error as error:
+        print('Ошибка при работе sql', error)
+    finally:
+        if con:
+            con.close()
+            print('Соединение с sql закрыто')
+
+def journals_insert(diary,dates_id,users_id):
+    try:
+        con= get_connection()
+        with con:
+            c = con.cursor()
+        c.execute('''
+                  INSERT INTO Journals(diary,dates_id,users_id) values
+                  (?,?,?);
+                  ''',(diary,dates_id,users_id,))
+        con.commit()
+    except sqlite3.Error as error:
+        print('Ошибка при работе sql', error)
+    finally:
+        if con:
+            con.close()
+            print('Соединение с sql закрыто')
+
+def users_select_all():
+    try:
+        con = get_connection()
+        with con:
+            c=con.cursor()
+        res = c.execute('''
+        SELECT * FROM Users
+        ''')
+        res = res.fetchall()
+        if not res:
+            print('No users')
+            return False
+        return res
+    except sqlite3.Error as error:
+        print('Error with SQLite in users_select_all',error)
+    finally:
+        if con:
+            con.close()
+            print('Connection closed')
+
+def users_select(login):
+    try:
+        con = get_connection()
+        with con:
+            c=con.cursor()
+        res = c.execute('''
+        SELECT * FROM Users
+        WHERE login = ?;
+        ''',(login,))
+        res = res.fetchall()
+        if not res:
+            print('No users')
+            return False
+        return res
+    except sqlite3.Error as error:
+        print('Error with SQLite in users_select',error)
+    finally:
+        if con:
+            con.close()
+            print('Connection closed')
+
+
+
+if __name__ == '__main__':
+    users()
+    dates()
+    journals()
+    # users_insert('Andrew', '1234')
+    # dates_insert('2024-03-01')
+    # journals_insert('test3', 3, 1)
+    # u = (users_select('Andrew'))
+    # print(u[0]['psw'])
+    # print([i for name in users_select_all() for i in name])
+
---
+++ routers/user.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+
+from db.db import users_select_all
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.get("/all")
+async def all_users():
+    r = users_select_all()
+    return {"status_code": status.HTTP_201_CREATED, "transaction": r}
